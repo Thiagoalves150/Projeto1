bda4fe71786104b39db54d9bdd068aa29a850525
thiago alves1 <thiago.alves150@etec.sp.gov.br>
2022-10-31 20:26:31 -0300

    salvando projeto

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int numero2;
+            int total;
+
+            numero = 69;
+            numero2 = 3;
+
+            total = numero / numero2;
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+
+    }
+}
